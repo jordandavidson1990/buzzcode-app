a68f6b462254e56478c981d8627331d8d075f9f9
Jordan Davidson <user@users-MacBook-Pro.local>
2019-02-14 14:14:06 +0000

    https

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) =>{
         <div className="upper-wrapper">
             <div className="logo">
             <h2  type="button" onClick={ props.refresh } id="buzzcode">BuzzCode</h2>
-            <img className="image-logo"src="http://batenborch.com/blog/wp-content/uploads/2015/07/summer-holiday-hd-wallpaper-cupcakepedia-1024x640.jpg" alt="img"></img>
+            <img className="image-logo"src="https://batenborch.com/blog/wp-content/uploads/2015/07/summer-holiday-hd-wallpaper-cupcakepedia-1024x640.jpg" alt="img"></img>
             </div>
         </div>
     )
